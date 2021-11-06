256ecb9b74802f87a965c4340e738baa63399cde
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-11-06 14:47:01 +0000

    Update index.svelte

--- index.svelte
+++ index.svelte
@@ -1,1 +1,45 @@
 <link rel="stylesheet" href="https://framework7.io/css/main.css"></link>
+<Page>
+  <Navbar title="Toggle"></Navbar>
+  <BlockTitle>Super Heroes</BlockTitle>
+  <List simpleList>
+    <ListItem>
+      <span>Batman</span>
+      <Toggle checked></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Aquaman</span>
+      <Toggle checked color="blue"></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Superman</span>
+      <Toggle checked color="red"></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Hulk</span>
+      <Toggle color="green"></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Spiderman (Disabled)</span>
+      <Toggle disabled></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Ironman (Disabled)</span>
+      <Toggle checked disabled></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Thor</span>
+      <Toggle checked color="orange"></Toggle>
+    </ListItem>
+    <ListItem>
+      <span>Wonder Woman</span>
+      <Toggle color="pink"></Toggle>
+    </ListItem>
+  </List>
+</Page>
+  
+
+
+<script>
+  import {Page, Navbar, BlockTitle, List, ListItem, Toggle} from 'framework7-svelte';
+</script>
